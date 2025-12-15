d52a5a234982811e6b1c0fd9bea00a3dbad9510c
Jefte Santos <jefte.dos.santos@gmail.com>
2025-12-15 16:35:27 -0300

    feat: implement initial Flutter shopping list app with core screens and data management

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'services/hive_service.dart';
-import 'examples/hive_example.dart';
+import 'screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:lista_compras/models/listas_provider.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveService.init();
+  Intl.defaultLocale = 'pt_BR';
   runApp(
     MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ListasProvider()),
-      ],
-      child: MyApp(),
-    ));
+      providers: [ChangeNotifierProvider(create: (_) => ListasProvider())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,10 +25,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lista de Compras',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        tabBarTheme: const TabBarThemeData(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white70,
+          indicatorColor: Colors.white,
+          dividerColor: Colors.transparent,
+        ),
       ),
-      home: const HiveExample(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('pt', 'BR')],
+      home: const HomeScreen(),
     );
   }
 }
