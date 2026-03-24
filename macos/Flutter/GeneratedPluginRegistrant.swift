7e3ce58b7385d1edfd3aeff836502e64806563da
Jefte Santos <jefte.dos.santos@gmail.com>
2026-03-23 21:08:18 -0300

    fix: remove Firebase (requer google-services.json), mantem splash e onboarding

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_crashlytics
 import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
 import printing
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
